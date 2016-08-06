b86fd90b409d58c5b3eed5b4a835b7ad109ad9eb
albyh <albyheredia@gmail.com>
2016-08-06 15:39:43 -0700

    Initial Commit

--- CU2/Controllers/HomeController.cs
+++ CU2/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CU2.Controllers
 {
@@ -26,5 +22,6 @@ namespace CU2.Controllers
 
             return View();
         }
+
     }
 }
