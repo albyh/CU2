01dcd5573ec1f37f140b09f6c196ce36fe393daf
albyh <albyheredia@gmail.com>
2016-08-06 17:55:31 -0700

    Created db context

---
+++ CU2/DAL/SchoolContext.cs
@@ -0,0 +1,24 @@
+﻿using CU2.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace CU2.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        //the connection string (added to the Web.config file later) is passed in to the constructor.
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //prevents table names from being pluralized.
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ CU2/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CU2.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        //Enrollments property is a navigation property.Navigation properties hold other entities that are related to this entity
+        //Navigation properties are typically defined as virtual so that they can take advantage of certain Entity Framework functionality such as lazy loading. 
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
